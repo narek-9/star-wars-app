6207d9299232ef0755bd7989f40445c18c7efe4b
Narek <narekazibekyan5@gmail.com>
2023-06-19 14:13:25 +0400

    Created Error HOC

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -4,31 +4,42 @@ import { getApiResource } from "../../utils/network";
 import { API_PEOPLE } from "../../constants/api";
 import { getPeopleId, getPeopleImage } from "../../services/getPeopleData";
 import PeopleList from "../../components/PeoplePage/PeopleList";
+import { withErrorApi } from "../../hoc-helpers/withErrorApi";
 
 import styles from "./PeoplePage.module.css";
 
-const PeoplePage = () => {
+const PeoplePage = ({ setErrorApi }) => {
   const [people, setpeople] = useState(null);
 
   const getResource = async (url) => {
     const res = await getApiResource(url);
-    const peopleList = res.results.map(({ name, url }) => {
-      const id = getPeopleId(url);
-      const img = getPeopleImage(id);
-      return {
-        id,
-        name,
-        img,
-      };
-    });
-    setpeople(peopleList);
+    if (res) {
+      const peopleList = res.results.map(({ name, url }) => {
+        const id = getPeopleId(url);
+        const img = getPeopleImage(id);
+        return {
+          id,
+          name,
+          img,
+        };
+      });
+      setpeople(peopleList);
+      setErrorApi(false);
+    } else {
+      setErrorApi(true);
+    }
   };
 
   useEffect(() => {
     getResource(API_PEOPLE);
   }, []);
 
-  return <>{people && <PeopleList people={people} />}</>;
+  return (
+    <>
+      <h1>Navigation</h1>
+      {people && <PeopleList people={people} />}
+    </>
+  );
 };
 
-export default PeoplePage;
+export default withErrorApi(PeoplePage);
