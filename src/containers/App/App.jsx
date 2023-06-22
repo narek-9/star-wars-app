0e31106e907029087e403f4f33b2bf20374dae2b
Narek <narekazibekyan5@gmail.com>
2023-06-23 01:42:36 +0400

    Added Routes, create and styled Header component

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,9 +1,29 @@
-import PeoplePage from "@containers/PeoplePage";
+import routesConfig from "@routes/routesConfig";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "@components/Header";
 
 import styles from "./App.module.css";
 
 const App = () => {
-  return <PeoplePage />;
+  return (
+    <>
+      <BrowserRouter>
+        <div className={styles.wrapper}>
+          <Header />
+
+          <Routes>
+            {routesConfig.map((route) => (
+              <Route
+                path={route.path}
+                element={route.element}
+                key={route.path}
+              />
+            ))}
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
